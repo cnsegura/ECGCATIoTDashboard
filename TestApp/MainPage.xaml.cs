b4e24a588196c57228a0c1905c6f1ef60d714f12
Chris Segura <chris.segura@live.com>
2016-02-18 21:12:41 -0800

    minor changes

--- TestApp/MainPage.xaml.cs
+++ TestApp/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using KafkaConsumer;
 using Windows.Foundation;
